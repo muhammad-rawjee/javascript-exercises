546b85d784973b8404dfb569ac54687659b5e65d
muhammad-rawjee <muhammadrawjee@gmail.com>
2023-07-11 12:42:04 -0400

    Solved

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(input) {
+    return input.split("").reverse().join("");
 };
 
 // Do not edit below this line
 module.exports = reverseString;
+
+console.log(reverseString("Ali was here"));
