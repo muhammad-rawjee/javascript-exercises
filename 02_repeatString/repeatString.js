1da0202910c7d926d279c5527b8ee6ce1fc43971
Muhammad Ali Rawjee <muhammadrawjee@gmail.com>
2025-03-05 14:01:18 +0530

    Fin'

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, rep) {
+    let res = ''
+    if (rep < 0){
+        return "ERROR";
+    }
+    for (let i = 0; i < rep; i++){
+        res += string;
+    }
+    return res;
 };
 
 // Do not edit below this line
