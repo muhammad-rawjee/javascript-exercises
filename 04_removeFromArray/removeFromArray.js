132747379e661d0f75ab6959bd7359d50c2b606d
Muhammad Ali Rawjee <muhammadrawjee@gmail.com>
2025-03-05 14:48:13 +0530

    Fin

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function (numArray, ...theArgs) {
+    const newArray = [];
+
+    numArray.forEach((element) => {
+        if (!theArgs.includes(element)){
+            newArray.push(element);
+        }
+    });
+
+    return newArray;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(min, max) {
+    if (min > max){
+        let temp = min;
+        min = max;
+        max = temp;
+    }
 
+    if ((min < 0) || (max < 0)){
+        return "ERROR"
+    }
+    else if (!Number.isInteger(min) || !Number.isInteger(max)){
+        return "ERROR" 
+    }
+    let res = 0;
+    for (let i = min; i <= max; i++){
+        res += i
+    }
+    return res
 };
 
 // Do not edit below this line
