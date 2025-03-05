6f6b0fecdd8fa7cac1f1c96a3661c28cb5d43d6e
Muhammad Ali Rawjee <muhammadrawjee@gmail.com>
2025-03-05 15:16:08 +0530

    Fin

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(faren) {
+  res = (faren - 32) * (5 / 9);
+  return Math.round(res * 10) / 10; 
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celc) {
+  res = (celc * (9 / 5) + 32);
+  return Math.round(res * 10) / 10;
 };
 
 // Do not edit below this line
